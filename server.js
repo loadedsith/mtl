ae727567f9fde01b9a6e0357e158026799a96cda
Graham P Heath <graham.p.heath@gmail.com>
2014-05-31 22:10:51 -0500

    sadf

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var T = new Twit({
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser());
-
+// fuck
 
 // var mongoose   = require('mongoose');
 // mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o'); // connect to our database
